56bd25d000b34d73e87889a2e2a77b615835e585
Cayetano Bañón <cayetano.banon@finetwork.com>
2023-02-20 11:20:02 +0100

    Equatable in Range values

--- lib/inputs/xlider_range_values.dart
+++ lib/inputs/xlider_range_values.dart
@@ -9,4 +9,27 @@ class XliderRangeValues {
 
   bool get isComplete => (min != null && max != null);
   bool get isEmtpy => (min != null && max != null);
+
+  @override
+  String toString() => 'XliderRangeValues(min: $min, max: $max)';
+
+  XliderRangeValues copyWith({
+    double? min,
+    double? max,
+  }) {
+    return XliderRangeValues(
+      min: min ?? this.min,
+      max: max ?? this.max,
+    );
+  }
+
+  @override
+  bool operator ==(covariant XliderRangeValues other) {
+    if (identical(this, other)) return true;
+
+    return other.min == min && other.max == max;
+  }
+
+  @override
+  int get hashCode => min.hashCode ^ max.hashCode;
 }
